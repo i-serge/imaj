1e28b576d15fca0f06e04295b92554f139258ef4
Steven Barragan <steven@toolsnet.mx>
2011-07-12 20:00:10 -0500

    Agregado controlado y vista index

---
+++ application/controllers/principal.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of principal
+ *
+ * @author steven
+ */
+class Principal extends CI_Controller{
+    
+    public function index(){
+        $this->load->view( 'index');
+    }
+}
+
+?>
